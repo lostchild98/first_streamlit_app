764232f79b0975d534d348fbcebfba09a2d28a34
lostchild98 <cecloclo@live.fr>
2022-08-23 10:29:41 +0200

    Add smileys

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
 streamlit.title('My parents new healthy dinner')
-streamlit.header('Breakfirst Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinnach and rocket smoothie')
-streamlit.text('Hard-boiled free-range eggs')
+streamlit.header('Breakfirst Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinnach and rocket smoothie')
+streamlit.text('🐔 Hard-boiled free-range eggs')
+streamlit.text('🥑🍞 Avocado toast')
