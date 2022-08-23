e16693cb2d7f230c2c0dc3db35682778c83d3538
lostchild98 <cecloclo@live.fr>
2022-08-23 10:26:49 +0200

    menu breakfirst

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents new healthy dinner')
+streamlit.header('Breakfirst Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinnach and rocket smoothie')
+streamlit.text('Hard-boiled free-range eggs')
