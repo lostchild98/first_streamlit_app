476967e1eadf0d3d615c50cfbc9e120cbf7f452f
lostchild98 <cecloclo@live.fr>
2022-08-23 10:47:43 +0200

    display table by fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,5 +13,6 @@ streamlit.text('🥑🍞 Avocado toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(my_fruit_list)
