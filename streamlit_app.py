a6b42c423ed80ec0cd818e3e5f4b5b1d5e68bd7b
lostchild98 <cecloclo@live.fr>
2022-08-24 00:51:12 +0200

    first try modify dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,4 +38,5 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit list contains:")
 streamlit.dataframe(my_data_rows)
 
-add_my_fruit = streamlit.multiselect("What fruit would you like to add ?")
+add_my_fruit = streamlit.text_input("What fruit would you like to add ?")
+streamlit.dataframe(my_data_rows).append(add_my_fruit)
