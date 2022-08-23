3e6fdeb20185e656832a588efc0c83a3bd5912b6
lostchild98 <cecloclo@live.fr>
2022-08-23 10:30:53 +0200

    smoothie header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit
 
 streamlit.title('My parents new healthy dinner')
+
 streamlit.header('Breakfirst Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinnach and rocket smoothie')
 streamlit.text('🐔 Hard-boiled free-range eggs')
 streamlit.text('🥑🍞 Avocado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
